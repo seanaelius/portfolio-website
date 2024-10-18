bb4eb35b02e733978922a4ee79f19c6cd88e2167
seanaelius <sdc.startup@gmail.com>
2024-10-17 20:23:43 -0400

    Add about me section Initiate skills section Add basic styling to about me section

--- src/components/home.js
+++ src/components/home.js
@@ -8,7 +8,7 @@ const home = (function() {
     // INTRODUCTION
     //====================================================================================================================================================================================================
     //DEFINE INTRO
-    const intro = document.createElement("div")
+    const intro = document.createElement("section")
     intro.classList.add("intro")
 
     //DEFINE LS, RS
@@ -22,7 +22,7 @@ const home = (function() {
     const bio = document.createElement("p")
     greetings.textContent = "Hi, my name is Sean Aelius"
     const abstract = document.createElement("h2")
-    abstract.textContent = "I'm a Software Engineer from Toronto, ON."
+    abstract.textContent = "I'm a Software Engineer located in Toronto, ON."
     bio.textContent = "I am a lifelong learner looking to apply software solutions to real-world problems."
 
 
@@ -44,8 +44,47 @@ const home = (function() {
     //ABOUT ME
     //====================================================================================================================================================================================================
     //DEFINE ABOUT ME SECTION
+    const about_me = document.createElement("section")
+    about_me.classList.add("about-me")
+    
+    const about_me_title = document.createElement("h2")
+    const about_me_para = document.createElement("p")
+    about_me_title.textContent = "About Me"
+    about_me_para.textContent = "I am a recent Mechanical Engineering graduate from TMU (formerly Ryerson), currently transitioning to become a Full Stack Software Developer. " 
+    + "With a strong foundation in engineering and a passion for software development, I am eager to apply my analytical skills to create innovative solutions in the tech industry. "
+    + "During my time as an undergrad, I very often found myself using MATLAB or Python to solve many of my lab reports and engineering assignments. "
+    + "This quickly turned into a personal hobby that I have continued to develop by creating various web applications. "
+    + 'Since then I have been following an online curriculum known as "The Odin Project", which has helped me develop the necessary foundations to become a software engineer. ' 
+
+    const about_me_para_2 = document.createElement("p")
+    about_me_para_2.textContent = ""
+
+   
+    //DEFINE TECH STACK
+
+    //ADD CONTENT TO ABOUT_ME
+    about_me.appendChild(about_me_title)
+    about_me.appendChild(about_me_para)
+    
+    
+ 
+
+
+    //ADD ABOUT_ME TO MAIN.CONTENT 
+    mainContent.appendChild(about_me)
+    
+    //====================================================================================================================================================================================================
+    //SKILLS
+    //====================================================================================================================================================================================================
+    //DEFINE SKILLS
+
+    //ADD CONTENT TO SKILLS
+
+    //ADD SKILLS TO MAIN.CONTENT
+
+
     /*
-    bio.textContent =  " I am a recent Mechanical Engineering graduate from TMU (formerly Ryerson), currently transitioning to become a Full Stack Software Engineer through an online curriculum known as The Odin Project. " 
+    " I am a recent Mechanical Engineering graduate from TMU (formerly Ryerson), currently transitioning to become a Full Stack Software Engineer through an online curriculum known as The Odin Project. " 
     + "With a strong foundation in engineering and a passion for software development, I am eager to apply my analytical skills to create innovative solutions in the tech industry. "
     */
     }  
