d8a4ddcdd95de6a42c2b318755a7caea0da374eb
seanaelius <sdc.startup@gmail.com>
2024-10-17 21:23:56 -0400

    Add skills section content

--- src/components/home.js
+++ src/components/home.js
@@ -53,22 +53,15 @@ const home = (function() {
     about_me_para.textContent = "I am a recent Mechanical Engineering graduate from TMU (formerly Ryerson), currently transitioning to become a Full Stack Software Developer. " 
     + "With a strong foundation in engineering and a passion for software development, I am eager to apply my analytical skills to create innovative solutions in the tech industry. "
     + "During my time as an undergrad, I very often found myself using MATLAB or Python to solve many of my lab reports and engineering assignments. "
-    + "This quickly turned into a personal hobby that I have continued to develop by creating various web applications. "
-    + 'Since then I have been following an online curriculum known as "The Odin Project", which has helped me develop the necessary foundations to become a software engineer. ' 
+    + "This quickly turned into a personal hobby where I continued to develop various applications. "
+    + 'Since then I have been following an online curriculum known as "The Odin Project", which has helped me learn the necessary foundations to become a software engineer. ' 
 
     const about_me_para_2 = document.createElement("p")
     about_me_para_2.textContent = ""
 
-   
-    //DEFINE TECH STACK
-
     //ADD CONTENT TO ABOUT_ME
     about_me.appendChild(about_me_title)
     about_me.appendChild(about_me_para)
-    
-    
- 
-
 
     //ADD ABOUT_ME TO MAIN.CONTENT 
     mainContent.appendChild(about_me)
@@ -77,6 +70,24 @@ const home = (function() {
     //SKILLS
     //====================================================================================================================================================================================================
     //DEFINE SKILLS
+    const skills = document.createElement("section")
+    skills.classList.add("skills")
+    const skills_title = document.createElement("h2")
+    skills_title.textContent = "Current Tech-Stack:"
+    skills.appendChild(skills_title)
+    
+    //DEFINE TECH STACK 
+    let tech_stack_list = ['html', 'css', 'js', 'react','node','git','python','r','matlab','sql',]
+    for (let language in tech_stack_list) {
+        let div = document.createElement("div")
+        div.textContent = `${tech_stack_list[language]}`
+        div.classList = `${tech_stack_list[language]}`
+        skills.appendChild(div)
+    }
+
+    mainContent.appendChild(skills)
+    
+
 
     //ADD CONTENT TO SKILLS
 
