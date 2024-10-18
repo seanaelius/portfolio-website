be68b3b09e0a58d8bde0b3bd0649a3d501367820
seanaelius <sdc.startup@gmail.com>
2024-10-18 18:48:21 -0400

    Add a list of projects to project.js

--- src/components/projects.js
+++ src/components/projects.js
@@ -3,9 +3,19 @@ import { mainContent } from "..";
 
 const projects = (function() {
     const add = () => {
+
         //====================================================================================================================================================================================================
         //PROJECTS
         //====================================================================================================================================================================================================
+
+        //ODIN ETCH-A-SKETCH
+        //ODIN ROCK-PAPER-SCISSORS (CLEAN THIS UP)
+        //ODIN TIC-TAC-TOE (CLEAN THIS UP)
+        //GENSHIN IMPACT WISH SIMULATOR
+        //MECH ENGINEERING PROJECT
+        //ODIN WEATHER APP
+        //ODIN TO-DO LIST
+        
     }  
     return { add } 
 })();
